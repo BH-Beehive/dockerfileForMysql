960c3989fab3c5624a3a0043bd3e9afa37ae2933
Mauricio Maxuel Afonso dos Santos <99891423+Mauricio-Maxuel@users.noreply.github.com>
2022-11-28 22:21:49 -0300

    Update createDb.sql

--- db/createDb.sql
+++ db/createDb.sql
@@ -6,7 +6,5 @@ id_registro int primary key auto_increment,
 data_registro timestamp default current_timestamp not null,
 memoria_uso double,
 cpu_uso double,
-disco_uso double,
-tipo_alerta varchar(15),
-check ( tipo_alerta = "VERDE" or tipo_alerta = "AMARELO" or tipo_alerta = "VERMELHO")
+disco_uso double
 );
